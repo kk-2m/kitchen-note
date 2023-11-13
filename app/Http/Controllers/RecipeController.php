72e4822efda8b9fc807b9d67ff9b4431811ae954
kk-2m <kishi.koki6@gmail.com>
2023-11-13 09:47:13 +0900

    RecipeController微修正

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -165,7 +165,6 @@ class RecipeController extends Controller
             // 元々登録されていた画像は削除
             if($recipe->image != null) Storage::disk('sftp')->delete($recipe->image);
             $file = $request->file('recipe.image');
-            // dd(is_readable(Storage::disk('sftp')->putFile($userId, $file)));
             if(!\Storage::disk('sftp')->exists($userId)) {
                 \Storage::disk('sftp')->makeDirectory($userId);
             }
